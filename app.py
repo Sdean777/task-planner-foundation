79f36bc632a950819236022e335d3767896ee844
Sdean777 <80027042+Sdean777@users.noreply.github.com>
2026-05-22 11:32:45 -0500

    Add health check endpoint
    Added a health check endpoint to return the online status.

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 def hello():
     return "Dean'z Elite OS Foundation Online"
 
+@app.route('/health')
+def health():
+    return {"status": "online"}
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
-    app.run(port=port,host='0.0.0.0')
+    app.run(port=port, host='0.0.0.0')
